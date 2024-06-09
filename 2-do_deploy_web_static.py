d60663eee27e369cadc528e257ec6a6231e307bb
JohnOngeri <j.ouma@alustudent.com>
2024-06-09 08:17:09 +0000

    added compressed web_static

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -6,7 +6,7 @@ archive to the web servers
 
 from fabric.api import put, run, env
 from os.path import exists
-env.hosts = ['54.89.109.87', '100.25.190.21']
+env.hosts = ['54.227.124.240', '54.226.52.168']
 
 
 def do_deploy(archive_path):
