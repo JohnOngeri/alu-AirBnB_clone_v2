1c75aa293b0c3d2befbd86eaeff75baa2add0166
JohnOngeri <144005852+JohnOngeri@users.noreply.github.com>
2024-06-13 11:32:01 +0200

    Update 9-states.py

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,42 +1,41 @@
 #!/usr/bin/python3
-'''
-Script runs flask web application listening on 0.0.0.0
-and port 5000
-'''
-from flask import Flask, render_template
-from models import storage
+""" this module contains a script that starts a Flask web application
+    the web application must be listening on 0.0.0.0, port 5000
+    Routes: - /states_list """
+from models import *
+from models.base_model import BaseModel, Base
+from models.user import User
+from models.place import Place
 from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
+classes = {"Amenity": Amenity, "City": City,
+           "Place": Place, "Review": Review, "State": State, "User": User}
+
+
 @app.route('/states', strict_slashes=False)
-def states_list():
-    '''
-    Default route for mimimal flask application
-    '''
-    states = storage.all(State)
-    return render_template("9-states.html", states=states, state=None)
-
-
 @app.route('/states/<id>', strict_slashes=False)
-def state(id):
-    '''
-    Default route for mimimal flask application
-    '''
-    states = storage.all(State)
-    try:
-        queried_state = states["State.{}".format(id)]
-    except Exception:
-        queried_state = ""
-    return render_template("9-states.html", state=queried_state)
+def states_by_id(id=None):
+    """ display HTML page with list of states """
+    all_states = storage.all(State)
+    if id:
+        states = all_states.get('State.{}'.format(id))
+    else:
+        states = all_states.values()
+    # ^ fetches states data from storage engine, then in line below,
+    # those states are passed into the template
+    return render_template('9-states.html', states=states)
 
 
 @app.teardown_appcontext
-def teardown(exception):
-    '''Tears down the current sqlalchemy session after each request
-    '''
+def remove_SQLalc_session(exception):
+    """ close storage when tear down is called """
     storage.close()
 
-
-if __name__ == "__main__":
-    app.run(port=5000, host="0.0.0.0")
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)  
